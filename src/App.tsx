674741d162c17d39cce1370f509928b1878fd4cd
Stephen Delaney <sdelaney@auxita.com>
2021-02-21 23:55:15 +1000

    Initial working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';import './App.css';
+import { SVGViewer } from './components/SVGViewer';
+import Icon from './Icon';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+
+  const [fileData, setFileData] = useState("");
+
+  const getBase64 = async (file: File): Promise<string | undefined>  => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result?.toString());
+      reader.onerror = (error) => reject(error);
+    });
+  };
+
+  const onUpload = async (event: any) => {
+    const file = event.target.files[0];
+    const uri = await getBase64(file);
+    if (uri) {
+      setFileData(uri);
+    }
+  }
+
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+      <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Laserify
+            <Icon />
+          </Typography>
+            <input
+              accept='*.svg'
+              type='file'
+              id='fileUpload'
+              onChange={(e) => onUpload(e)}
+            />
+        </Toolbar>       
+      </AppBar>
+      {fileData && (
+        <SVGViewer fileData={fileData}/>
+      )}
     </div>
   );
 }
